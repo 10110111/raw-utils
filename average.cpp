1447e9de7e46c8a2e6f4ecff099717ad05a96e4e
Ruslan Kabatsayev <b7.10110111@gmail.com>
2019-01-12 17:05:32 +0300

    Add an option to return error on over- or underexposure when calculating average color

--- average.cpp
+++ average.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <sstream>
 #include <cstddef>
+#include <cstring>
 #include <vector>
 #include <limits>
 #include <cassert>
@@ -12,11 +13,13 @@ using std::size_t;
 
 int usage(const char* argv0, int returnValue)
 {
-    std::cerr << "Usage: " << argv0 << " filename\n";
+    std::cerr << "Usage: " << argv0 << " filename [--error-on-misexposure]\n";
     return returnValue;
 }
 
-void printAverageColor(LibRaw& libRaw, const ushort (*img)[4], const int w, const int h,
+bool errorOnMisexposure=false;
+
+bool printAverageColor(LibRaw& libRaw, const ushort (*img)[4], const int w, const int h,
                        libraw_colordata_t const& colorData, const float (&rgbCoefs)[4])
 {
     const auto rgbCoefR =rgbCoefs[0];
@@ -24,8 +27,10 @@ void printAverageColor(LibRaw& libRaw, const ushort (*img)[4], const int w, cons
     const auto rgbCoefB =rgbCoefs[2];
     const auto rgbCoefG2=rgbCoefs[3];
 
+    bool misexposure=false;
     double red=0, green1=0, green2=0, blue=0;
     int tooBlackPixelCount=0, tooWhitePixelCount=0;
+    int blackPixelCount=0, whitePixelCount=0;
     for(int y=0;y<h;++y)
     {
         for(int x=0;x<w;++x)
@@ -41,6 +46,14 @@ void printAverageColor(LibRaw& libRaw, const ushort (*img)[4], const int w, cons
             {
                 ++tooWhitePixelCount;
                 pixelRaw=colorData.maximum;
+            }
+            if(pixelRaw==colorData.black)
+            {
+                ++blackPixelCount;
+            }
+            else if(pixelRaw==colorData.maximum)
+            {
+                ++whitePixelCount;
             }
 
             const auto pixel=pixelRaw-colorData.black;
@@ -59,9 +72,25 @@ void printAverageColor(LibRaw& libRaw, const ushort (*img)[4], const int w, cons
     green2/=w*h;
     blue/=w*h;
     if(tooBlackPixelCount)
+    {
         std::cerr << "Warning: " << tooBlackPixelCount << " pixels have values less than black level\n";
+        misexposure=true;
+    }
     if(tooWhitePixelCount)
+    {
         std::cerr << "Warning: " << tooWhitePixelCount << " pixels have values greater than white level\n";
+        misexposure=true;
+    }
+    if(blackPixelCount && !tooBlackPixelCount)
+    {
+        std::cerr << "Warning: " << blackPixelCount << " pixels are underexposed\n";
+        misexposure=true;
+    }
+    if(whitePixelCount && !tooWhitePixelCount)
+    {
+        std::cerr << "Warning: " << whitePixelCount << " pixels are overexposed\n";
+        misexposure=true;
+    }
     std::cout << "Mean camera R,G1,G2,B: " << red << ',' << green1 << ',' << green2 << ',' << blue << '\n';
     const auto green=(green1+green2)/2;
     const auto& cam2srgb=colorData.rgb_cam;
@@ -75,13 +104,22 @@ void printAverageColor(LibRaw& libRaw, const ushort (*img)[4], const int w, cons
     const auto Z=0.0193*srgblR + 0.1192*srgblG + 0.9505*srgblB;
     std::cout << "XYZ: " << X << ',' << Y << ',' << Z << '\n';
     std::cout << "xyY: " << X/(X+Y+Z) << ',' << Y/(X+Y+Z) << ',' << Y << '\n';
+
+    return errorOnMisexposure && misexposure;
 }
 
 int main(int argc, char** argv)
 {
-    if(argc!=2)
+    if(argc!=2 && argc!=3)
         return usage(argv[0],1);
     const char* filename=argv[1];
+    if(argc==3)
+    {
+        if(!std::strcmp(argv[2],"--error-on-misexposure"))
+            errorOnMisexposure=true;
+        else
+            return usage(argv[0],1);
+    }
     LibRaw libRaw;
     libRaw.open_file(filename);
     const auto& sizes=libRaw.imgdata.sizes;
@@ -98,6 +136,6 @@ int main(int argc, char** argv)
     const auto& cam_mul=libRaw.imgdata.color.cam_mul;
     const float camMulMax=*std::max_element(std::begin(cam_mul),std::end(cam_mul));
     const float rgbCoefs[4]={cam_mul[0]/camMulMax,cam_mul[1]/camMulMax,cam_mul[2]/camMulMax,cam_mul[3]/camMulMax};
-    printAverageColor(libRaw,libRaw.imgdata.image,sizes.iwidth,sizes.iheight,libRaw.imgdata.color,rgbCoefs);
+    return printAverageColor(libRaw,libRaw.imgdata.image,sizes.iwidth,sizes.iheight,libRaw.imgdata.color,rgbCoefs);
 }
 
