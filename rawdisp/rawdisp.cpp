827c9ba484159c49d73e2087e553445345225420
Ruslan Kabatsayev <b7.10110111@gmail.com>
2021-10-19 01:07:15 +0300

    Make organization name 10110111 to have a predictable path to settings

--- rawdisp/rawdisp.cpp
+++ rawdisp/rawdisp.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
+    app.setOrganizationName("10110111");
     MainWindow mainWin(argv[1] ? argv[1] : "");
     mainWin.show();
     return app.exec();
