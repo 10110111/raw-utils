2083a86cdc181b8672a0925333a503004cee421b
Ruslan Kabatsayev <b7.10110111@gmail.com>
2023-03-05 16:56:40 +0400

    Reset demosaic message state together with demosaiced image readiness

--- rawdisp/ImageCanvas.cpp
+++ rawdisp/ImageCanvas.cpp
@@ -24,6 +24,7 @@ static QSurfaceFormat makeFormat()
 void ImageCanvas::openFile(QString const& filename)
 {
     demosaicedImageReady_=false;
+    demosaicMessageShown_=false;
     emit warning("");
     emit loadingFile(filename);
     libRaw.reset();
